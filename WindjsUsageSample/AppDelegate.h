0838ae4f6d6d029ec11a25dabff01f51f37491ff
Tang Qiao <tangqiaoboy@gmail.com>
2012-08-28 21:14:15 +0800

    Initial Commit

---
+++ WindjsUsageSample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WindjsUsageSample
+//
+//  Created by TangQiao on 12-8-28.
+//  Copyright (c) 2012年 TangQiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ WindjsUsageSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WindjsUsageSample
+//
+//  Created by TangQiao on 12-8-28.
+//  Copyright (c) 2012年 TangQiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
